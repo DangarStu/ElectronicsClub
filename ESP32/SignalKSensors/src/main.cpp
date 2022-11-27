52e36ce0421d8c1f7205f973dad8b82ed2043c61
Stuart Allen <dangarstu@gmail.com>
2022-11-27 15:34:14 +1100

    Fixed bug with reversed sample values.

--- ESP32/SignalKSensors/src/main.cpp
+++ ESP32/SignalKSensors/src/main.cpp
@@ -18,6 +18,7 @@
 #include "sensesp/transforms/curveinterpolator.h"
 #include "sensesp/transforms/linear.h"
 #include "sensesp/transforms/voltagedivider.h"
+#include "sensesp/transforms/moving_average.h"
 
 using namespace sensesp;
 
@@ -31,17 +32,19 @@ class TemperatureInterpreter : public CurveInterpolator {
 
     // Seed the CurveInterpolator with the resistances and temperatures from the
     // sensor datasheet.
-    float data_points[23][2] = {
+    float data_points[29][2] = {
       {10, 5728}, {15, 4496}, {20, 3555}, {25, 2830}, {30, 2268}, {35, 1828}, {40, 1483},
       {45, 1210}, {50, 992}, {55, 819}, {60, 679}, {65, 566}, {70, 475}, {75, 400},
       {80, 338}, {85, 287}, {90, 244.8}, {95, 209.7}, {100, 180.3}, {105, 155.6},
-      {110, 134.7}, {115, 117.1}, {120, 102.2}
+      {110, 134.7}, {115, 117.1}, {120, 102.2}, {125, 89.4}, {130, 78.5}, {135, 69.1}, 
+      {140, 61.1}, {145, 54.1}, {150, 48.1}
     };
 
     clear_samples();
 
-    for (int index=0; index < 23; index++) {
-      add_sample(CurveInterpolator::Sample(data_points[index][0], data_points[index][1]));
+    for (int index=0; index < 29; index++) {
+      add_sample(CurveInterpolator::Sample(data_points[index][1], data_points[index][0]));
+      debugD("Sample(%F,%F)", data_points[index][1], data_points[index][0]);
     }
   }
 };
@@ -60,31 +63,27 @@ void setup() {
                     // Optionally, hard-code the WiFi and Signal K server
                     // settings. This is normally not needed.
                     ->set_wifi("D-Link DVA-2800", "Landrover")
-                    //->set_sk_server("192.168.10.3", 80)
+                    ->set_sk_server("192.168.1.3", 3000)
                     ->get_app();
 
-  // GPIO number to use for the analog input
+  // GPIO number to use for the coolant sensor input
   const uint8_t collantAnalogInputPin = 36;
 
   // Voltage sent into the voltage divider circuit that includes the analog
   // sender
   const float Vin = 3.3;
+
   // The resistance, in ohms, of the fixed resistor (R1) in the voltage divider
   // circuit
   const float R1 = 330.0;
 
-  // Define how often (in milliseconds) new samples are acquired
-  const unsigned int kAnalogInputReadInterval = 500;
-
-  // Define the produced value at the maximum input voltage (3.3V).
-  // A value of 3.3 gives output equal to the input voltage.
-  const float kAnalogInputScale = 4096;
+  auto* data_buffer = new MovingAverage(20, 1.0, "");
 
   // An AnalogInput gets the value from the microcontroller's AnalogIn pin,
   // which is a value from 0 to 1023.
   // ESP32 has many pins that can be used for AnalogIn, and they're
   // expressed here as the XX in GPIOXX.
-  auto* analog_input = new AnalogInput(collantAnalogInputPin);
+  auto* analog_input = new AnalogInput(collantAnalogInputPin, 200, "", 1024);
 
     // The "Signal K path" identifies the output of this sensor to the Signal K
   // network.
@@ -103,9 +102,10 @@ void setup() {
      - send calibrated Kelvin value to Signal K with SKOutputNumber()
   */
   analog_input->connect_to(new AnalogVoltage())
+      ->connect_to(data_buffer)
       ->connect_to(new VoltageDividerR2(R1, Vin, "/coolant/temp/sender"))
       ->connect_to(new TemperatureInterpreter("/coolant/temp/curve"))
-      ->connect_to(new Linear(1.0, 273.0, "/collant/temp/calibrate"))
+      // ->connect_to(new Linear(1.0, 273.0, "/collant/temp/calibrate"))
       ->connect_to(
           new SKOutputFloat(sk_path, "/coolant/temp/sk"));
 
@@ -116,7 +116,7 @@ void setup() {
   // Read GPIO 14 every time it changes
   const uint8_t highWaterAlarmDigitalInputPin = 14;
   auto* highWaterSwitch =
-      new DigitalInputChange(highWaterAlarmDigitalInputPin, INPUT_PULLUP, CHANGE);
+      new DigitalInputChange(highWaterAlarmDigitalInputPin, INPUT_PULLDOWN, CHANGE);
 
   // Connect the digital input to a lambda consumer that prints out the
   // value every time it changes.
@@ -141,7 +141,7 @@ void setup() {
 
   // Configure the pin. Replace this with your custom library initialization
   // code!
-  pinMode(kDigitalInput2Pin, INPUT_PULLUP);
+  pinMode(kDigitalInput2Pin, INPUT_PULLDOWN);
 
   // Define a new RepeatSensor that reads the pin every 100 ms.
   // Replace the lambda function internals with the input routine of your custom
